ef0a89de6f226c5c7762b6137e1715c79867fcd7
Wouter <wdedood@outlook.com>
2024-01-09 14:52:54 +0100

    new version

--- Get-ChildManagementGroup.ps1
+++ Get-ChildManagementGroup.ps1
@@ -1,20 +1,72 @@
 function Get-ChildManagementGroup
 {
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$managementGroupName
+        [Parameter(Mandatory = $false)]
+        [string]$managementGroupID
     )
 
-    foreach ($managementGroup in Get-AzManagementGroup -GroupName $managementGroupName -Expand)
+    begin
     {
-        $getChildManagementGroups = $managementGroup.Children | Where-Object { $_.Type -ne "/subscriptions" }
-        Write-Host "$($managementGroup.Name) [ $(($getChildManagementGroups.Children).Count) ]"
-        if (($getChildManagementGroups.Children).Count -gt 0)
+        $returnValue = @()
+        $rootManagementGroupIsSet = $false
+    }
+    process
+    {
+        #
+        # If no management group is provided, find the root management group and set the management group ID
+        #
+        if ($managementGroupID -eq "")
         {
-            foreach ($child in $getChildManagementGroups)
+            Write-Verbose "Find root management group as no groupName is provided"
+            $allManagementGroups = Get-AzManagementGroup
+            do
             {
-                Get-ChildManagementGroup -managementGroupName $child.Name
+                foreach ($managementGroup in $allManagementGroups)
+                {
+                    $checkManagementGroup = $(Get-AzManagementGroup -GroupName $($managementGroup.Name) -Expand)
+                    Write-Verbose "Checking $(($checkManagementGroup).Name)"
+                    if ($null -eq $(($checkManagementGroup).ParentName))
+                    {
+                        Write-Verbose "Setting management ID to [ $(($checkManagementGroup).Name) ]"
+                        $managementGroupID = $($checkManagementGroup.Name)
+                        $rootManagementGroupIsSet = $true
+                        #
+                        # Leave the foreach loop if the root management group is found
+                        #
+                        break
+                    }
+                }
+            } while ($rootManagementGroupIsSet -eq $false)
+            
+        }
+        Write-Verbose "Get management group [ $($managementGroupID) ]"
+        $allManagementGroups = Get-AzManagementGroup -GroupName $managementGroupID -Expand
+        #
+        # Loop through all management groups and check if they have children
+        #
+        foreach ($managementGroup in $allManagementGroups)
+        {
+            Write-Verbose "Checking [ $(($managementGroup).Name) ] for children"
+            $getChildManagementGroupCount = @($managementGroup.Children | Where-Object { $_.Type -eq "Microsoft.Management/managementGroups" }).Count
+            if ($getChildManagementGroupCount -gt 0)
+            {
+                Write-Verbose "Found children [ $($getChildManagementGroupCount) ] for [ $(($managementGroup).Name) ]"
+                $returnValue = $returnValue + $managementGroup
+                $allChildren = $managementGroup.Children | Where-Object { $_.Type -eq "Microsoft.Management/managementGroups" }
+                foreach ($child in $allChildren)
+                {
+                    Get-ChildManagementGroup -managementGroupID $child.Name
+                }
+            }
+            else
+            {
+                $returnValue = $returnValue + $managementGroup
+                Write-Verbose "No children found for [ $(($managementGroup).Name) ]"
             }
         }
+    }
+    end
+    {
+        return $returnValue
     }
 }
